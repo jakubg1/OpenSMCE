0434b98eea3643c67c3d036db9745a42e900f1df
jakubg1 <jakub.gronostaj@wp.pl>
2021-01-24 17:51:41 +0100

    Lightning beam now utilizes the destroyParticle field
    This means that this particle is no longer hardcoded.

--- src/Shooter.lua
+++ src/Shooter.lua
@@ -147,7 +147,7 @@ function Shooter:shoot()
 	local sphereConfig = game.spheres[self.color]
 	if sphereConfig.shootBehavior.type == "lightning" then
 		-- lightning spheres are not shot, they're deployed instantly
-		game:spawnParticle("particles/lightning_beam.json", self:spherePos())
+		game:spawnParticle(sphereConfig.destroyParticle, self:spherePos())
 		game.session:destroyVerticalColor(self.pos.x, sphereConfig.shootBehavior.range, self.color)
 		game.session.level.combo = 0 -- cuz that's how it works
 	else
