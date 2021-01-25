a70df3fce9e9c4ef06834b96fe6ab4a4fbf7780a
jakubg1 <jakub.gronostaj@wp.pl>
2021-01-25 13:47:29 +0100

    Revert shooting spheres ending lightning storm

--- src/Shooter.lua
+++ src/Shooter.lua
@@ -157,8 +157,8 @@ function Shooter:shoot()
 	game:playSound(sphereConfig.shootSound)
 	self:setColor(0)
 	game.session.level.spheresShot = game.session.level.spheresShot + 1
-	game.session.level.lightningStormCount = 0
-	game.session.level.lightningStormTime = 0
+	--game.session.level.lightningStormCount = 0
+	--game.session.level.lightningStormTime = 0
 end
 
 
