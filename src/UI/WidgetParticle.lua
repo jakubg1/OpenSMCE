d8369a15c976e0c52db5aeed169ce696703614d9
jakubg1 <jakub.gronostaj@wp.pl>
2021-08-04 22:24:26 +0200

    Add new particle despawn widget action
    When a widget is a particle, and that particle effect has finished (it despawned completely), a `particleDespawn` action is executed.

--- src/UI/WidgetParticle.lua
+++ src/UI/WidgetParticle.lua
@@ -6,19 +6,24 @@ local ParticleManager = require("src/Particle/Manager")
 
 function UIWidgetParticle:new(parent, path)
 	self.type = "particle"
-	
+
 	self.parent = parent
 	self.path = path
-	
+
 	self.manager = ParticleManager()
+	self.packet = nil
 end
 
 function UIWidgetParticle:update(dt)
 	self.manager:update(dt)
+	if self.packet and self.packet.delQueue then
+		self.packet = nil
+		self.parent:executeAction("particleDespawn")
+	end
 end
 
 function UIWidgetParticle:spawn()
-	self.manager:spawnParticlePacket(self.path, self.parent:getPos())
+	self.packet = self.manager:spawnParticlePacket(self.path, self.parent:getPos())
 end
 
 function UIWidgetParticle:despawn()
