49449d79c2bbd7c4c379ac112f691170024adf62
jakubg1 <jakub.gronostaj@wp.pl>
2021-08-13 17:53:06 +0200

    Add adjustable highscore size and fix clear bug
    Highscores could be cleared only once per launch. Also added `size` field to highscore config.

--- src/Highscores.lua
+++ src/Highscores.lua
@@ -3,6 +3,7 @@ local Highscores = class:derive("Highscores")
 
 function Highscores:new(data)
 	self.data = data
+	self.config = game.configManager.highscores
 
 	-- default if not found
 	if not self.data then self:reset() end
@@ -13,8 +14,15 @@ end
 function Highscores:reset()
 	print("Resetting Highscores...")
 
-	self.data = {}
-	self.data.entries = game.configManager.highscores.default_scores
+	self.data = {entries = {}}
+	for i = 1, self.config.size do
+		local def = self.config.default_scores[i]
+		self.data.entries[i] = {
+			name = def.name,
+			score = def.score,
+			level = def.level
+		}
+	end
 end
 
 function Highscores:getEntry(n)
@@ -23,17 +31,17 @@ end
 
 function Highscores:getPosition(score)
 	-- nil if it does not qualify
-	for i = 10, 1, -1 do
+	for i = self.config.size, 1, -1 do
 		local entry = self:getEntry(i)
 		if score <= entry.score then
-			if i == 10 then return nil else return i + 1 end
+			if i == self.config.size then return nil else return i + 1 end
 		end
 	end
 	return 1
 end
 
 function Highscores:storeProfile(profile, pos)
-	for i = 9, pos, -1 do
+	for i = self.config.size - 1, pos, -1 do
 		-- everyone who is lower than the new highscore goes down
 		self.data.entries[i + 1] = self:getEntry(i)
 	end
