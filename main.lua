1e08057c1045b226dced7730768199565a5c3fac
jakubg1 <jakub.gronostaj@wp.pl>
2022-03-07 14:44:08 +0100

    Update version number to Beta 4.2.2

--- main.lua
+++ main.lua
@@ -21,8 +21,8 @@ local DiscordRichPresence = require("src/DiscordRichPresence")
 
 
 -- CONSTANT ZONE
-_VERSION = "v0.42.1"
-_VERSION_NAME = "Beta 4.2.1"
+_VERSION = "v0.42.2"
+_VERSION_NAME = "Beta 4.2.2"
 _DISCORD_APPLICATION_ID = "797956172539887657"
 
 
