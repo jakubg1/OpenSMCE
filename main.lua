4791fa397cfcaa15260909558ed953973e9c0250
jakubg1 <jakub.gronostaj@wp.pl>
2022-08-15 13:36:59 +0200

    Bump version to Beta 4.6.0

--- main.lua
+++ main.lua
@@ -25,8 +25,8 @@ local DiscordRichPresence = require("src/DiscordRichPresence")
 
 
 -- CONSTANT ZONE
-_VERSION = "v0.45.0"
-_VERSION_NAME = "Beta 4.5.0"
+_VERSION = "v0.46.0"
+_VERSION_NAME = "Beta 4.6.0"
 _DISCORD_APPLICATION_ID = "797956172539887657"
 
 
