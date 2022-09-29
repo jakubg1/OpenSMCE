0f88935c276c5cefedb1da8d1a203a107fd1de96
jakubg1 <jakub.gronostaj@wp.pl>
2022-09-30 01:18:51 +0200

    Document Sprite.lua

--- src/Essentials/Sprite.lua
+++ src/Essentials/Sprite.lua
@@ -37,6 +37,12 @@ function Sprite:new(path)
 	end
 end
 
+
+
+---Returns a quad object for use in drawing functions.
+---@param state integer The state ID of this sprite.
+---@param frame Vector2 The sprite to be obtained.
+---@return love.Quad
 function Sprite:getFrame(state, frame)
 	--print(self.frames[(frame.x - 1) % self.frameCount.x + 1][(frame.y - 1) % self.frameCount.y + 1])
 	--for k1, v1 in pairs(self.frames) do
@@ -46,6 +52,17 @@ function Sprite:getFrame(state, frame)
 	return s.frames[(frame.x - 1) % s.frameCount.x + 1][(frame.y - 1) % s.frameCount.y + 1]
 end
 
+
+
+---Draws this Sprite onto the screen.
+---@param pos Vector2 The sprite position.
+---@param align Vector2? The sprite alignment. `(0, 0)` is the top left corner. `(1, 1)` is the bottom right corner. `(0.5, 0.5)` is in the middle.
+---@param state integer? The state ID to be drawn.
+---@param frame Vector2? The sprite to be drawn.
+---@param rot number? The sprite rotation in radians.
+---@param color Color? The sprite color.
+---@param alpha number? Sprite transparency. `0` is fully transparent. `1` is fully opaque.
+---@param scale Vector2? The scale of this sprite.
 function Sprite:draw(pos, align, state, frame, rot, color, alpha, scale)
 	align = align or Vec2()
 	state = state or 1
@@ -62,5 +79,7 @@ function Sprite:draw(pos, align, state, frame, rot, color, alpha, scale)
 	end
 	self.img:draw(self:getFrame(state, frame), pos.x, pos.y, rot, scale.x * _GetResolutionScale(), scale.y * _GetResolutionScale())
 end
+
+
 
 return Sprite
