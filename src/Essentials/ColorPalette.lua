e708b74e4ad821d5d5bd6b497cff5ac8b3bc1890
jakubg1 <jakub.gronostaj@wp.pl>
2022-11-06 16:19:44 +0100

    Remove unnecessary diagnostics in ColorPalette

--- src/Essentials/ColorPalette.lua
+++ src/Essentials/ColorPalette.lua
@@ -11,13 +11,13 @@ local Color = require("src/Essentials/Color")
 
 function ColorPalette:new(path)
 	self.data = _LoadImageData(path)
-	if not self.data then error("Failed to load image data: " .. path) end
----@diagnostic disable-next-line: undefined-field
+	if not self.data then
+		error("Failed to load image data: " .. path)
+	end
 	self.size = Vec2(self.data:getDimensions())
 end
 
 function ColorPalette:getColor(t)
----@diagnostic disable-next-line: undefined-field
 	return Color(self.data:getPixel(t % self.size.x, 1))
 end
 
